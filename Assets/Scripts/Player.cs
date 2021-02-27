fe9311ade7ed3420a57f40f99db44304910ae594
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-02-26 22:32:11 -0300

    versionando projeto

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public bool IsJumping;
+    public bool DoubleJump;
+    public bool isBlowing;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        // Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        // move personagem em uma direcao
+        // transform.position += movement * Time.deltaTime * Speed;
+
+        float movement = Input.GetAxis("Horizontal");
+        // utiliza fisica da Engine para movimentar
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if (movement > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if (movement < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if (movement == 0)
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && !isBlowing)
+        {
+            if(!IsJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                DoubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if(DoubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    DoubleJump = false;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            IsJumping = false;
+            anim.SetBool("jump", false);
+        }
+
+        if (collision.gameObject.tag == "Spike")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Saw")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            IsJumping = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == 11)
+        {
+            isBlowing = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == 11)
+        {
+            isBlowing = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    public float speed;
+    public float movementTime;
+
+    private bool directionRight = true;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (directionRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= movementTime)
+        {
+            directionRight = !directionRight;
+            timer = 0f;
+        }
+    }
+}
